9d8ddd25031f3a4f9a92046531ba9ee8d094d46f
Sirojiddin Mirzayev <79570217+Mirzayevsky@users.noreply.github.com>
2024-05-13 09:47:32 -0700

    completed

--- app.js
+++ app.js
@@ -2,49 +2,148 @@ const createForm = document.getElementById("todo-form");
 const table = document.querySelector(".table");
 const taskDelete = document.querySelector(".task-delete");
 const time = new Date();
-
-let storageData = JSON.parse(localStorage.getItem("data"))
+const editForm = document.getElementById('modal')
+const overlay = document.querySelector('.overlay')
+const closeModal = document.getElementById('modal-close')
+let editItem 
+let todoData = JSON.parse(localStorage.getItem("data"))
   ? JSON.parse(localStorage.getItem("data"))
   : [];
 
-function setTodos() {
-  localStorage.setItem("data", JSON.stringify(storageData));
+if(todoData.length){
+  showTodos()
 }
 
-storageData.map((item) => {
-  table.innerHTML += `
- <div class="table-bottom">
-            <p class="table-Id">${item.id}</p>
-            <p class="Task-title">${item.title}</p>
-            <p class="task-date">${item.date}</p>
-            <p class="task-edit">Edit</p>
-            <p class="task-delete">Delete</p>
-        </div>
- `;
-});
-// localStorage.clear()
-// adding task function
+// set items inside localStorage
+function setTodos() {
+  localStorage.setItem("data", JSON.stringify(todoData));
+}
+
+// it returns error message
+function showMessage (where,message) {
+  const error = document.getElementById(`${where}`).value = message
+  console.log(error)
+  setTimeout(()=>{
+  document.getElementById(`${where}`).value = ""
+ },2000)
+}
+
+
+// adding loop tasks function
+function showTodos () {
+    const todos = JSON.parse(localStorage.getItem('data'))
+    table.innerHTML  = ''
+    todos?.forEach((item,i)=>{
+    table.innerHTML += `
+    <div class="table-bottom" >
+    <p class="table-Id">${i}</p>
+    <p class="Task-title">${item.title}</p>
+    <p class="task-date">${item.date}</p>
+    <div class="action-wrapper">
+     <img onclick="editTask(${i})" src="./assets/icons/8666681_edit_icon.svg" class="task-edit-btn"/>
+     <img  onclick="deleteItem(${i})"  src="./assets/icons/185090_delete_garbage_icon.svg" class="task-delete-btn"/>
+     </div>
+</div>
+    `
+  })
+}
+
+// add task function
 createForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const taskInput = createForm["task-input"].value.trim();
   const date = `${time.getHours()}:${time.getMinutes()}`;
-  let id = storageData.length + 1;
-  console.log(typeof taskInput)
-  if (taskInput.length > 0) {
-    let object = {
+  let id = todoData.length + 1
+  createForm.reset()
+  if (taskInput.length) {
+    todoData.push({
       id: id,
       title: taskInput,
       date: `${date}`,
       completed: false,
-    };
-    storageData.push(object);
+    });
     setTodos();
-    window.location.reload();
-
+    showTodos()
+  } else {
+    showMessage('task-input','Pease enter text...')
   }
 });
 
-// // deleting task function
-// taskDelete.addEventListener("click", () => {
-//   console.log("function worked");
-// });
+
+
+// edit function
+
+function open(){
+  overlay.classList.remove('hidden')
+}
+function close (){
+  overlay.classList.add('hidden')
+}
+
+closeModal.addEventListener('click',() => {
+ close()
+})
+
+
+function editTask(id) {
+ editItem = id
+ console.log(editItem)
+ open()
+}
+
+
+editForm.addEventListener('submit',(e) => {
+  e.preventDefault();
+  const editInput = editForm["modal-input"].value.trim();
+  const date = `${time.getHours()}:${time.getMinutes()} time:${time.getSeconds()}`;
+  editForm.reset()
+  if (editInput.length) {
+    todoData.splice(editItem,1,{
+      title:`${editInput}`,
+      date:`${date}`,
+      completed:false
+    })
+    
+    setTodos()
+    showTodos()
+    close()
+
+  } else {
+    showMessage('modal-input','Edit Your Text')
+  }
+})
+
+//delete task function
+
+
+
+// // delete task function
+function deleteItem(id){
+  console.log(id)
+ const deletedItems = todoData.filter((item,i)=>{
+   return i !== id
+ })
+ 
+ todoData = deletedItems
+ setTodos()
+ showTodos()
+}
+
+
+// complete task  function
+// const setCompleted = (id) => {
+//   console.log(id)
+//   const completedTasks  =  todoData.map((item) => {
+//     if(item.id === id){
+//       return {...item,completed: item.completed === true ? false : true}
+//     }else{
+//       return {...item}
+//     }
+//   })
+
+//   todoData = completedTasks
+//   setTodos()
+//   showTodos()
+  
+// }
+
